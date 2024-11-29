2304dd3cf22206474a1763697db18e7a6598c428
abdullahdurmuss <abdullah.durmus@raven.com.tr>
2024-11-29 16:54:54 +0300

    line added

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,7 +1,7 @@
 const apiUrl = process.env.NEXT_PUBLIC_API_URL
 
 async function getCategoriesByCompanySlug({ slug, lang = "en" }) {
-    const response = await fetch(`${apiUrl}/categories?company=${slug}&pagination[pageSize]=999&pagination[page]=1&populate[image][fields][0]=*&populate[company][populate][0]=logo&populate[company][populate][1]=theme&locale=${lang}`, {
+    const response = await fetch(`${apiUrl}/categories?sort[0]=line:asc&company=${slug}&pagination[pageSize]=999&pagination[page]=1&populate[image][fields][0]=*&populate[company][populate][0]=logo&populate[company][populate][1]=theme&locale=${lang}`, {
         cache: 'no-store'
     })
     const data = await response.json()
