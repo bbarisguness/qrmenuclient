eeb2c4f4bd7033af6b6ec1c9ce01f2c69860592d
abdullahdrms <abdullah.drms4@gmail.com>
2025-06-23 19:01:04 +0300

    sort change

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -9,7 +9,7 @@ async function getCategoriesByCompanySlug({ slug, lang = "en" }) {
 }
 
 async function getCategoriesByCompanySlug2({ slug, lang = "en" }) {
-    const response = await fetch(`${apiUrl}/categories?sort[0]=line:asc&company=${slug}&pagination[pageSize]=999&pagination[page]=1&populate[image][fields][0]=*&populate[company][populate][0]=logo&populate[products][populate][0]=image&populate[company][populate][1]=theme&locale=${lang}`, {
+    const response = await fetch(`${apiUrl}/categories?sort[0]=line:asc&company=${slug}&pagination[pageSize]=999&pagination[page]=1&populate[image][fields][0]=*&populate[company][populate][0]=logo&populate[products][sort][0]=line:asc&populate[products][populate][image][fields][0]=*&populate[company][populate][1]=theme&locale=${lang}`, {
         cache: 'no-store'
     })
     const data = await response.json()
