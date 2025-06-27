2c86ca3b599d816788ba63d79a8a4d430d8607fe
abdullahdrms <abdullah.drms4@gmail.com>
2025-06-27 12:50:58 +0300

    notfound css

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,16 +3,13 @@ import { getCompanyHome } from '@/services/companyService'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
-
-// export const metadata = {
-//   title: 'GDMENU | NotFound',
-//   description: 'Gdmenu ile işletmenizin sınırlarını zorlayın',
-// }
+import Head from 'next/head'
+import styles from './notFound.module.css'
 
 export default function NotFound() {
   const path = usePathname()
   const [link, setLink] = useState('')
-  
+
   useEffect(() => {
     getCompanyHome({ slug: path?.split('/')[1], lang: 'tr' }).then((res) => {
       if (res?.data?.[0]) {
@@ -24,9 +21,19 @@ export default function NotFound() {
   }, [])
 
   return (
-    <div className='flex flex-col items-center mt-[10px]'>
-      <div>Sayfa Bulunamadı</div>
-      <Link className='underline' href={link}>Anasayfa'ya dön</Link>
-    </div>
+    <>
+      <Head>
+        <title>Gdmenu | Sayfa Bulunamadı</title>
+      </Head>
+      <div className={`${styles.container} font-Poppins`}>
+        <div className={styles.content}>
+          <div className={styles.errorContent}>
+            <h1 className={styles.text}>404</h1>
+            <h3 className={styles.subtitle}>Sayfa Bulunamadı</h3>
+            <Link className={styles.button} href={link}>Anasayfa'ya Dön</Link>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
